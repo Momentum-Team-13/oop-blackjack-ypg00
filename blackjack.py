ffcf8140014da21306026b13bdcea4836ced36a0
Yuriy Golota <ypgolota@pm.me>
2022-06-23 20:37:32 -0400

    shuffle deck

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,11 @@
+import random
+import copy
+
 class Card():
-    def __init__(self, rank, suit, value):
+    def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        self.value = value
+        self.value = None
     
     def __str__(self):
         return f'{self.rank} {self.suit} (+{self.value})'
@@ -11,6 +14,7 @@ class Deck():
     def __init__(self):
         self.cards = []
         self.create_deck()
+        self.shuffle()
         
     def __str__(self):
         return f'{self.cards}'
@@ -19,14 +23,14 @@ class Deck():
         RANKS_VALUES = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': [1, 11]}
         SUITS = ['♣️', '♦️', '♥️', '♠️']
 
-        # Loop to creat the deck        
         for suit in SUITS:
-            for rank in RANKS_VALUES:
-                card = Card(rank, suit, rank.value())
+            for card in RANKS_VALUES:
+                card = Card(card, suit)
                 self.cards.append(card)
         
-    def suffle():
-        pass
+    def shuffle(self):
+        return random.shuffle(self.cards) 
+
     def deal():
         pass
     def draw():
@@ -45,10 +49,19 @@ class Player():
         pass
 
 def play_game():
+    # Create a deck
     deck = Deck()
     deck.create_deck()
-    for card in deck.cards:
+    # for card in deck.cards:
+    #     print(card)
+
+    # Suffle the deck
+    deck_shuffled = copy.deepcopy(deck)
+    deck_shuffled.shuffle()
+    
+    for card in deck_shuffled.cards:
         print(card)
+
 
 if __name__ == "__main__":
     play_game()
